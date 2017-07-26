61ae65c6401945f4932a6b894a473cfb33b0ba46
rabindrachhanchan <rabindra.chhanchan@mountblue.io>
2017-07-26 15:32:40 +0530

    dynamic rendering

--- app/components/App.js
+++ app/components/App.js
@@ -8,6 +8,7 @@ var Home = require('./Home');
 var Battle = require('./Battle');
 var Popular = require('./Popular');
 var Hello=require('./Hello');
+var Results=require('./Results');
 
 class App extends React.Component {
   render() {
@@ -19,6 +20,7 @@ class App extends React.Component {
           <Switch>
             <Route exact path='/' component={Home} />
             <Route exact path='/battle' component={Battle} />
+            <Route path='/battle/results' component={Results} />
             <Route path='/popular' component={Popular} />
             <Route path='/hello'   component={Hello} />
             <Route render={function () {
---
+++ app/components/Results.js
@@ -0,0 +1,10 @@
+var React=require('react');
+
+class Results extends React.Component{
+    render(){
+        return(
+           <div> Result </div>
+           )
+    }
+}
+module.exports=Results;
